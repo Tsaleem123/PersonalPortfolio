e90558cad9c72d69e6ab5d93706a9d59cbecc255
Talha Saleem <t_sal@hotmail.com>
2023-05-27 22:09:37 -0400

    Update on website.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,13 +2,31 @@
  * Defines a simple Navbar for the project.
  */
 export const Navbar = () => {
-  
   return (
-    <nav>
-      <section className = "right-section">
-      <a href="https://pdfhost.io/v/~uozVrr8k_MSalResDec" target = "_blank"> <h1>Resume</h1></a>
-      <a href = "mailto: t_sal@hotmail.com"><h1>Email</h1></a>
-      <a href="https://github.com/Tsaleem123/" target ="_blank"><h1>Github</h1></a>
+    <nav className="bg-navColor">
+      <section className="flex justify-between">
+      <h2 className="my-1 ml-5 mr-1 hidden sm:inline">Muhammad Saleem</h2>
+<h2 className="my-1 ml-5 mr-1 inline sm:hidden">Muhammad S.</h2>
+
+        <div className="flex">
+          <a href="https://pdfhost.io/v/8FoIwKkeI_MSalResNew" target="_blank">
+            <button className="bg-customColor hover:bg-black text-black hover:text-white font-bold py-1 px-2 my-1 border border-black rounded mr-1 text-sm">
+              Resume
+            </button>
+          </a>
+
+          <a href="mailto:t_sal@hotmail.com">
+            <button className="bg-customColor hover:bg-black text-black hover:text-white font-bold py-1 px-2 my-1 border border-black rounded text-sm">
+              Email
+            </button>
+          </a>
+
+          <a href="https://github.com/Tsaleem123/" target="_blank">
+            <button className="bg-customColor hover:bg-black text-black hover:text-white font-bold py-1 px-2 my-1 border border-black rounded text-sm">
+              Github
+            </button>
+          </a>
+        </div>
       </section>
     </nav>
   );
