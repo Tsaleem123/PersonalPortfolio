a9614e7eb4fe76fc9f537416f7761cce2f4febfe
Talha Saleem <t_sal@hotmail.com>
2023-05-29 17:05:33 -0400

    Responsive improvement.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,31 +3,31 @@
  */
 export const Navbar = () => {
   return (
-    <nav className="bg-navColor">
-      <section className="flex justify-between">
-      <h2 className="my-1 ml-5 mr-1 hidden sm:inline">Muhammad Saleem</h2>
-<h2 className="my-1 ml-5 mr-1 inline sm:hidden">Muhammad S.</h2>
+<nav className="bg-navColor">
+  <section className="flex justify-between">
+    <h2 className="my-1 ml-5 mr-1 hidden sm:inline text-sm sm:text-base">Muhammad Saleem</h2>
+    <h2 className="my-1 ml-5 mr-1 inline sm:hidden text-sm sm:text-base">Muhammad S.</h2>
 
-        <div className="flex">
-          <a href="https://pdfhost.io/v/8FoIwKkeI_MSalResNew" target="_blank">
-            <button className="bg-customColor hover:bg-black text-black hover:text-white font-bold py-1 px-2 my-1 border border-black rounded mr-1 text-sm">
-              Resume
-            </button>
-          </a>
+    <div className="flex">
+      <a href="https://pdfhost.io/v/8FoIwKkeI_MSalResNew" target="_blank">
+        <button className="bg-customColor hover:bg-black text-black hover:text-white font-bold py-1 px-2 my-1 border border-black rounded text-xs sm:text-sm">
+          Resume
+        </button>
+      </a>
 
-          <a href="mailto:t_sal@hotmail.com">
-            <button className="bg-customColor hover:bg-black text-black hover:text-white font-bold py-1 px-2 my-1 border border-black rounded text-sm">
-              Email
-            </button>
-          </a>
+      <a href="mailto:t_sal@hotmail.com">
+        <button className="bg-customColor hover:bg-black text-black hover:text-white font-bold py-1 px-2 my-1 border border-black rounded text-xs sm:text-sm">
+          Email
+        </button>
+      </a>
 
-          <a href="https://github.com/Tsaleem123/" target="_blank">
-            <button className="bg-customColor hover:bg-black text-black hover:text-white font-bold py-1 px-2 my-1 border border-black rounded text-sm">
-              Github
-            </button>
-          </a>
-        </div>
-      </section>
-    </nav>
+      <a href="https://github.com/Tsaleem123/" target="_blank">
+        <button className="bg-customColor hover:bg-black text-black hover:text-white font-bold py-1 px-2 my-1 border border-black rounded text-xs sm:text-sm">
+          Github
+        </button>
+      </a>
+    </div>
+  </section>
+</nav>
   );
 };
