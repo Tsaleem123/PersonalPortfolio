c505e91162ed018ea4d99bd2089d2cfee46dfc89
Talha Saleem <t_sal@hotmail.com>
2023-07-24 19:26:18 -0400

    Changed order of projects.

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -11,11 +11,11 @@ export const Projects = () => {
     <a href={MV.BooksAPILink} target="_blank">
     <Project Title = {MV.BooksAPITitle} Para = {MV.BooksAPIPara} Img = {MV.BooksAPIImg} TechList = {MV.BookAPITechList}/>
     </a>
-    <a href={MV.CalcLink} target="_blank">
-    <Project Title = {MV.CalcTitle} Para = {MV.CalcPara} Img = {MV.CalcImg} TechList = {MV.CalcTechList}/>
-    </a>
     <a href={MV.RUCafeLink} target="_blank">
     <Project Title = {MV.RUCafeTitle} Para = {MV.RUCafePara} Img = {MV.RUCafeImg} TechList = {MV.RUCafechList}/>
+    </a>
+    <a href={MV.CalcLink} target="_blank">
+    <Project Title = {MV.CalcTitle} Para = {MV.CalcPara} Img = {MV.CalcImg} TechList = {MV.CalcTechList}/>
     </a>
     <a href={MV.POWLink} target="_blank">
     <Project Title = {MV.POWTitle} Para = {MV.POWPara} Img = {MV.POWImg} TechList = {MV.POWList}/>
