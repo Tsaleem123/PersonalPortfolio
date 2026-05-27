0f0952d8ab362a8d28c62f5a37655e05d10b7e7f
tsaleem123 <t_sal@hotmail.com>
2026-05-27 19:50:10 -0400

    More vercel fixes. target="_blank" needed

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -8,13 +8,13 @@ export const Projects = () => {
   return (
     <section className = "Projects">
     <h1> Projects: </h1>
-    <a href={MV.GameInventoryLink} target="_blank">
+    <a href={MV.GameInventoryLink} target="_blank" rel="noreferrer" >
     <Project Title = {MV.GameInventoryTitle} Para = {MV.GameInventoryPara} Img = {MV.GameInventoryImg} TechList = {MV.GameInventoryList}/>
     </a>
-    <a href={MV.RUCafeLink} target="_blank">
+    <a href={MV.RUCafeLink} target="_blank" rel="noreferrer">
     <Project Title = {MV.RUCafeTitle} Para = {MV.RUCafePara} Img = {MV.RUCafeImg} TechList = {MV.RUCafechList}/>
     </a>
-    <a href={MV.POWLink} target="_blank">
+    <a href={MV.POWLink} target="_blank" rel="noreferrer">
     <Project Title = {MV.POWTitle} Para = {MV.POWPara} Img = {MV.POWImg} TechList = {MV.POWList}/>
     </a>
     </section>
